ce4a4812b23351be3df6491e86e979fec9bf02b2
Dave <123788157+parodave@users.noreply.github.com>
2025-07-22 13:09:06 +0700

    feat: expose FrameTicker named export

--- scripts/fixes/frame-ticker-patched.js
+++ scripts/fixes/frame-ticker-patched.js
@@ -1,4 +1,4 @@
-export default class FrameTicker {
+export class FrameTicker {
   constructor(callback) {
     this.callback = callback;
     this.running = false;
@@ -20,3 +20,4 @@ export default class FrameTicker {
     this.rafId = null;
   }
 }
+export default FrameTicker;
