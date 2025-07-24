a311f6cbbc943f12c39a3666db1283b9fc9d8121
Dave <123788157+parodave@users.noreply.github.com>
2025-07-25 00:05:17 +0700

    Remove travel map and country features

--- src/utils/terrainGenerator.ts
+++
@@ -1,9 +0,0 @@
-export const generateTerrain = (width: number, depth: number) => {
-  const blocks: [number, number, number][] = [];
-  for (let x = 0; x < width; x++) {
-    for (let z = 0; z < depth; z++) {
-      blocks.push([x, 0, z]);
-    }
-  }
-  return blocks;
-};
