1c2565448b8597d159b42e8934e373d9135fd637
Dave <123788157+parodave@users.noreply.github.com>
2025-07-24 22:44:42 +0700

    Move visitedCountries data file into src and update import

--- src/components/CountryGlobe.tsx
+++ src/components/CountryGlobe.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import * as THREE from 'three';
-import countries from '../../data/visitedCountries.json';
+import countries from '../data/visitedCountries.json';
 
 const CountryGlobe = () => {
   const globeRef = useRef<THREE.Mesh>(null);
