d836774a4a28638525907e2ff288670904942a44
Dave <123788157+parodave@users.noreply.github.com>
2025-08-22 21:33:58 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Karim Hammouche - Portfolio</h1>
-    </main>
-  )
-}
