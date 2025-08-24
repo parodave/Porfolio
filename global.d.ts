ce60a24eee0bb7398594e40c9ec4e6ada2cf9439
Dave <123788157+parodave@users.noreply.github.com>
2025-08-24 21:51:47 +0700

    Delete global.d.ts

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.module.css';
-declare module '*.css';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg' { const content: any; export default content; }
